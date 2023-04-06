487101fd9b1f1da637215eebf530145917ec9ec2
Vaibhav Matere <vpm00718897@gmail.com>
2023-04-06 18:56:44 +0530

    user Schema updated

--- model/user.js
+++ model/user.js
@@ -1,9 +1,24 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    select: false,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export const User = mongoose.model("User", schema);
